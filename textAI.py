bb02bed19a6a2be8be2fcbdff8b72f4710ec812c
Murat <144884820+Mikarmk@users.noreply.github.com>
2024-04-08 02:16:41 +0300

    Update textAI.py

--- textAI.py
+++ textAI.py
@@ -2,41 +2,39 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-text_data = [
-    "The quick brown fox jumps over the lazy dog",
-    "She sells seashells by the seashore"
-]
+def tokenize(text_data):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(text_data)
+    total_words = len(tokenizer.word_index) + 1
+    return tokenizer, total_words
 
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(text_data)
-total_words = len(tokenizer.word_index) + 1
+def preprocess_data(text_data, tokenizer, max_sequence_length):
+    input_sequences = []
+    for line in text_data:
+        token_list = tokenizer.texts_to_sequences([line])[0]
+        for i in range(1, len(token_list)):
+            n_gram_sequence = token_list[:i+1]
+            input_sequences.append(n_gram_sequence)
+    input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, padding='pre')
+    X, y = input_sequences[:,:-1],input_sequences[:,-1]
+    y = tf.keras.utils.to_categorical(y, num_classes=total_words)
+    return X, y
 
-input_sequences = []
-for line in text_data:
-    token_list = tokenizer.texts_to_sequences([line])[0]
-    for i in range(1, len(token_list)):
-        n_gram_sequence = token_list[:i+1]
-        input_sequences.append(n_gram_sequence)
+def build_model(total_words, max_sequence_length):
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Embedding(total_words, 100, input_length=max_sequence_length-1))
+    model.add(tf.keras.layers.LSTM(150))
+    model.add(tf.keras.layers.Dense(total_words, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
-max_sequence_length = max([len(seq) for seq in input_sequences])
-input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, padding='pre')
+def train_model(X, y, model, epochs):
+    model.fit(X, y, epochs=epochs, verbose=1)
 
-X, y = input_sequences[:,:-1],input_sequences[:,-1]
-y = tf.keras.utils.to_categorical(y, num_classes=total_words)
-
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Embedding(total_words, 100, input_length=max_sequence_length-1))
-model.add(tf.keras.layers.LSTM(150))
-model.add(tf.keras.layers.Dense(total_words, activation='softmax'))
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-model.fit(X, y, epochs=100, verbose=1)
-
-def generate_text(seed_text, next_words, model, max_sequence_len):
+def generate_text(seed_text, next_words, model, tokenizer, max_sequence_length):
     for _ in range(next_words):
         token_list = tokenizer.texts_to_sequences([seed_text])[0]
-        token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+        token_list = pad_sequences([token_list], maxlen=max_sequence_length-1, padding='pre')
         predicted = model.predict_classes(token_list, verbose=0)
         output_word = ""
         for word, index in tokenizer.word_index.items():
@@ -46,7 +44,19 @@ def generate_text(seed_text, next_words, model, max_sequence_len):
         seed_text += " " + output_word
     return seed_text
 
+text_data = [
+    "The quick brown fox jumps over the lazy dog",
+    "She sells seashells by the seashore"
+]
+
+tokenizer, total_words = tokenize(text_data)
+max_sequence_length = 10
+X, y = preprocess_data(text_data, tokenizer, max_sequence_length)
+
+model = build_model(total_words, max_sequence_length)
+train_model(X, y, model, epochs=100)
+
 seed_text = "brown fox"
 next_words = 5
-generated_text = generate_text(seed_text, next_words, model, max_sequence_length)
+generated_text = generate_text(seed_text, next_words, model, tokenizer, max_sequence_length)
 print(generated_text)
